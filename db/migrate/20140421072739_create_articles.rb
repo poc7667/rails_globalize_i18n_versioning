c5a2aa65d3204d9ea05c34da914d80685a5b8424
poc7667 <poc7667@gmail.com>
2014-04-21 15:35:25 +0800

    Add setting for 'globalize'. Should both modify model.rb and migration file
    Signed-off-by: poc7667 <poc7667@gmail.com>

--- db/migrate/20140421072739_create_articles.rb
+++ db/migrate/20140421072739_create_articles.rb
@@ -1,10 +1,20 @@
 class CreateArticles < ActiveRecord::Migration
-  def change
+  def up
     create_table :articles do |t|
       t.string :name
       t.integer :user_id
-
       t.timestamps
     end
+
+    Article.create_translation_table!({
+      name: :string,
+      content: :text
+    }, {
+      migrate_data: true
+    })
+  end
+
+  def down
+    Article.drop_translation_table! migrate_data: true
   end
 end
